c8f81d520b05a1cda4afb795c5e7b9288d24a8e1
nicoleonard <nikolasleonard@gmail.com>
2023-02-02 19:27:07 -0300

    Creación de métodos (comportamientos de las clases)     Palabra clave this     Constructores de las clases     Encapsulamiento     Propuesta de atributos privados en clases     Retorno de resultados con return

--- index.js
+++ index.js
@@ -2,27 +2,57 @@
 class Cliente{
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
+    rutCliente;
+
+    constructor(){
+        this.dniCliente='';
+        this.rutCliente='';
+    }
+
+}
+
+class CuentaCorriente{
+    numero;
+    #saldo;
+    agencia;
+
+    constructor(){
+        this.#saldo=0;
+        this.numero='';
+        this.agencia='';
+    }
+
+    depositoEnCuenta(valor){
+        if(valor > 0){
+            this.#saldo =+valor;
+            return this.#saldo;
+        }
+
+    }
+    retirarDeCuenta(valor){
+        if(valor <= this.#saldo){
+            this.#saldo -= valor;
+            return this.#saldo;
+        }
+
+    }
+
+    verSaldo(){
+        return this.#saldo;
+    }
 }
 
 const cliente1 = new Cliente();
 cliente1.nombreCliente = "Jose";
-cliente1.numeroCuenta = "124512512";
-cliente1.dniCliente = "123124";
-cliente1.saldoCuenta = "500";
+
 
 const cliente2 = new Cliente();
 cliente2.nombreCliente = "Pepe";
-cliente2.numeroCuenta = "124512242";
-cliente2.dniCliente = "12312244";
-cliente2.saldoCuenta = "50000000";
+
 
 const cliente3 = new Cliente();
 cliente3.nombreCliente = "Fernanda";
-cliente3.numeroCuenta = "124512522";
-cliente3.dniCliente = "12341242";
-cliente3.saldoCuenta = "500000000000";
+
 
 console.log(cliente1);
 console.log(cliente2);
